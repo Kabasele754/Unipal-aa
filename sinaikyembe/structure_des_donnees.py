50bbede04baba4b9109a4b8ffbe7151e9694dc54
sinaikyembe <sinaikyembe26@gmail.com>
2023-05-23 15:59:07 +0200

    modification du fichier structure de donnees

--- sinaikyembe/structure_des_donnees.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 10 12:58:36 2023
-
-@author: ianis
-"""
-
---
+++ sinaikyembe/structure_donnees.py
@@ -0,0 +1,14 @@
+list_identite=["kyembe","samba","sanai"]
+list_identite.append("elise")
+list_identite.insert(1, "chatie")
+print(list_identite)
+for i in list_identite:
+    print(i)
+print(list_identite)
+print(list_identite[0])
+dic_ville={"hk":["likasi","kasumbalesa"]}
+print(dic_ville.values())
+print(dic_ville.keys())
+tuple_art=("jupe"," chemise","pantalon","polo", "chemise", "jupe")
+print(tuple_art.count("jupe"))
+print(tuple_art[0::2])
